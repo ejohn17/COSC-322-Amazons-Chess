cb9a6a4b9dcf51bbe9d757b130c9534719f2a911
GrandTheftWalrus <70998757+GrandTheftWalrus@users.noreply.github.com>
2021-06-03 01:38:41 -0700

    Create GameState.java

---
+++ AmazonsMain/src/main/java/ubc/cosc322/GameState.java
@@ -0,0 +1,44 @@
+package ubc.cosc322;
+
+import java.util.ArrayList;
+
+public class GameState {
+	private int[] action; //The action taken to get to this game state from its parent
+	private Board board;
+	private double value = 0;
+	private ArrayList<GameState> children = new ArrayList<>();;
+	private boolean childrenGenerated = false;
+	
+	public GameState(int[] action, GameState parent) {
+		this.action = action;
+		board = parent.getBoard();
+		board.movePiece(action);
+	}
+	
+	public GameState(Board board) {
+		this.action = null;
+		this.board = board;
+	}
+	
+	/** @return The action taken to get to this GameState from its parent GameState. */
+	public double getValue() { return value; }
+	
+	/** @return The action taken to get to this GameState from its parent GameState. */
+	public int[] getAction() { return action; }
+	
+	/**@return The board configuration that this GameState represents. */
+	public Board getBoard() { return board; }
+	
+	/** Returns a list of this GameState's child states. Internal ArrayList not generated until the first time this method is called.
+	 * @param team The number of the team for which you want all possible moves, aka the child nodes
+	 * @return An ArrayList containing all the GameStates that can be transitioned to from this GameState in one action.
+	 */
+	public ArrayList<GameState> getChildren(int team) {
+		if (!childrenGenerated) {
+			for (int[] action : board.getAllPossibleMoves(team))
+				children.add(new GameState(action, this));
+			childrenGenerated = true;
+		}
+		return children;
+	}
+}
